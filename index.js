c1853299795395ad88411d930e81fd66133cfda6
Melissa Real <melissa.real@hotmail.com>
2019-12-26 14:40:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+/* This is a file inserted to keep the learn IDE browser happy */
+
+var testVar  = {}
+
+let glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+  };
+  
+  let colorStates = {
+    "red" : "",
+    "": "red"
+  };
+
+  let articleHearts = document.querySelectorAll(".like-glyph");
+
+  function likeCallback(e) {
+    let heart = e.target;
+    mimicServerCall()
+      .then(function(){
+       
+        heart.innerText = glyphStates[heart.innerText];
+        heart.style.color = colorStates[heart.style.color];
+      })
+      .catch(function(error) {
+        alert("Something went wrong!");
+      });
+  }
+
+
+for (let glyph of articleHearts) {
+    glyph.addEventListener("click", likeCallback);
+  }
+  
+
+
+function mimicServerCall() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve("Pretend remote server notified of action!");
+      }, 300);
+    });
+  }
